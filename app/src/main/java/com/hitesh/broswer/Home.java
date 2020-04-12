8c682e82fd0df127bd7b0b4d72a6cdb31ece38e1
hiteshhv <46933160+hiteshhv@users.noreply.github.com>
2020-04-12 13:33:42 +0530

    Get URL data from urlTextFieldHome(MainActivity.Java) and pass to urlField(Home.java)
    Signed-off-by: hiteshhv <46933160+hiteshhv@users.noreply.github.com>

--- app/src/main/java/com/hitesh/broswer/Home.java
+++ app/src/main/java/com/hitesh/broswer/Home.java
@@ -2,17 +2,125 @@ package com.hitesh.broswer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.webkit.CookieManager;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import java.util.Objects;
 
 public class Home extends AppCompatActivity {
 
+    EditText urlField;
+    ImageButton goBack;
+    ImageButton goForward;
+    WebView webView;
+    Button enter;
+    String text;
+
+    @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         Objects.requireNonNull(this.getSupportActionBar()).hide();
 
+
+        urlField = (EditText) findViewById(R.id.urlField);
+        goBack = (ImageButton) findViewById(R.id.goBack);
+        goForward = (ImageButton) findViewById(R.id.goForward);
+        webView = (WebView) findViewById(R.id.webView);
+        enter = (Button) findViewById(R.id.enter);
+
+        if (savedInstanceState != null) {
+            webView.restoreState(savedInstanceState);
+        } else {
+            webView.getSettings().setJavaScriptEnabled(true);
+            webView.getSettings().setUseWideViewPort(true);
+            webView.getSettings().setLoadWithOverviewMode(true);
+            webView.getSettings().setSupportZoom(true);
+            webView.getSettings().setSupportMultipleWindows(true);
+            webView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+            webView.setBackgroundColor(Color.WHITE);
+
+        }
+        webView.setWebViewClient(new Home.MyWebViewClient());
+
+        Intent intent = getIntent();
+        text = intent.getStringExtra(MainActivity.Extra_Text);
+        launchURL();
     }
+    public void launchURL(){
+
+        try {
+            if(!Home.NetworkState.connectionAvailable(Home.this)){
+            }else {
+
+                InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                inputMethodManager.hideSoftInputFromWindow(urlField.getWindowToken(), 0);
+                webView.loadUrl("https://" + text);
+                urlField.setText("");
+            }
+
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void goTo(View v) {
+
+        try {
+            if(!Home.NetworkState.connectionAvailable(Home.this)){
+            }else {
+
+                InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                inputMethodManager.hideSoftInputFromWindow(urlField.getWindowToken(), 0);
+                webView.loadUrl("https://" + urlField.getText().toString());
+                urlField.setText("");
+            }
+
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public void goBack(View view){
+        webView.goBack();
+    }
+
+    public void goForward(View view){
+        webView.goForward();
+    }
+    public static class MyWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            CookieManager.getInstance().setAcceptCookie(true);
+            return true;
+        }
+    }
+    public static class NetworkState {
+
+        static boolean connectionAvailable(Context context){
+
+            ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+            assert connectivityManager != null;
+            return connectivityManager.getActiveNetworkInfo() !=null;
+        }
+    }
+
+
 }
--- app/src/main/java/com/hitesh/broswer/MainActivity.java
+++ app/src/main/java/com/hitesh/broswer/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Color;
 import android.net.ConnectivityManager;
 import android.os.Bundle;
@@ -21,83 +22,28 @@ import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText urlField;
-    ImageButton goBack;
-    ImageButton goForward;
-    WebView webView;
-    Button enter;
 
-    @SuppressLint("SetJavaScriptEnabled")
-    @Override
+    Button goToHome;
+    EditText urlTextFieldHome;
+    public static final String Extra_Text = "com.hitesh.broswer.Extra_Text";
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Objects.requireNonNull(this.getSupportActionBar()).hide();
 
+        goToHome = (Button) findViewById(R.id.goToHome);
+        urlTextFieldHome = (EditText) findViewById(R.id.urlTextFieldHome);
+       }
+       public void openHomeActivity(View view){
 
-        urlField = (EditText) findViewById(R.id.urlField);
-        goBack = (ImageButton) findViewById(R.id.goBack);
-        goForward = (ImageButton) findViewById(R.id.goForward);
-        webView = (WebView) findViewById(R.id.webView);
-        enter = (Button) findViewById(R.id.enter);
-
-        if (savedInstanceState != null) {
-            webView.restoreState(savedInstanceState);
-        } else {
-            webView.getSettings().setJavaScriptEnabled(true);
-            webView.getSettings().setUseWideViewPort(true);
-            webView.getSettings().setLoadWithOverviewMode(true);
-            webView.getSettings().setSupportZoom(true);
-            webView.getSettings().setSupportMultipleWindows(true);
-            webView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
-            webView.setBackgroundColor(Color.WHITE);
-
-        }
-        webView.setWebViewClient(new MyWebViewClient());
-
-    }
-
-    public void goTo(View v) {
-
-        try {
-            if(!NetworkState.connectionAvailable(MainActivity.this)){
-            }else {
-
-                InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-                inputMethodManager.hideSoftInputFromWindow(urlField.getWindowToken(), 0);
-                webView.loadUrl("https://" + urlField.getText().toString());
-                urlField.setText("");
-            }
-
-        }catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+        String urlTextHome = urlTextFieldHome.getText().toString();
+        Intent intent = new Intent(this, Home.class);
+        intent.putExtra(Extra_Text, urlTextHome);
+        startActivity(intent);
+       }
 
 
-    public void goBack(View view){
-        webView.goBack();
-    }
 
-    public void goForward(View view){
-        webView.goForward();
-    }
-    static class MyWebViewClient extends WebViewClient {
-
-        @Override
-        public boolean shouldOverrideUrlLoading(WebView view, String url) {
-            view.loadUrl(url);
-            CookieManager.getInstance().setAcceptCookie(true);
-            return true;
-        }
-    }
-    public static class NetworkState {
-
-        static boolean connectionAvailable(Context context){
-
-            ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
-            assert connectivityManager != null;
-            return connectivityManager.getActiveNetworkInfo() !=null;
-        }
-    }
 }
+
